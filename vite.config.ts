e4c12617943fa769acdc04b05b948a026b639900
小枫 <51731411+Fengjing95@users.noreply.github.com>
2025-04-14 11:23:36 +0800

    ci: 更新base路径

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/project-preview',
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
